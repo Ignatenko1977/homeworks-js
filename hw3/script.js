abf9e75ff5f43b0f6f6e023a787c7ab7658c332d
Ignatenko1977 <Provokattor@i.ua>
2022-10-03 09:58:17 +0300

    add homework3

---
+++ hw3/script.js
@@ -0,0 +1,65 @@
+// const userName = prompt("как твое имя");
+// console.log(`userName`, userName)
+// alert("Hello " + userName + " !")
+
+// let userMoneyAmount = 500;
+
+// const amountToAdd = Number(prompt("How much to add?"));
+// userMoneyAmount = userMoneyAmount + amountToAdd;
+
+// alert("Now in your wallet: " + userMoneyAmount);
+
+// const isAdmin = confirm("Are you admin?");
+// console.log(`isAdmin`, isAdmin);
+
+// if (isAdmin) {
+//     alert("Welcome");
+// }
+
+/*-----------Lesson-1-------------------*/
+
+
+let oneNumber;
+oneNumber = Number(prompt(" введите число"));
+console.log(`oneNumber`, oneNumber)
+
+let twoNumber;
+twoNumber = Number(prompt(" введите число"));
+console.log(`twoNumber`, twoNumber)
+
+alert("Вы ввели числа: " + oneNumber + " и " + twoNumber);
+
+if (oneNumber > twoNumber) {
+    alert("число " + oneNumber + " больше,чем число " + twoNumber);
+}
+if (oneNumber < twoNumber) {
+    alert("число " + twoNumber + " больше,чем число " + oneNumber);
+}
+
+if (oneNumber === twoNumber) {
+    alert('Числа равны');
+}
+
+/*--------------Lesson-2----------*/
+
+// const currentYears = 2021;
+
+// const userAnswer = Number(prompt("Какой сейчас год?"));
+// if (userAnswer === currentYears) {
+//     alert("Вы правы!");
+// } else {
+//     alert("А вот и нет :(");
+// }
+
+// const MIN_AGE = 12;
+// const visitorAge = Number(prompt("Сколько вам лет?"));
+// const isWithParents = confirm("Вы с родителями?");
+
+// if (visitorAge >= MIN_AGE || isWithParents) {
+//     console.log("Доступ разрешен!");
+// } else {
+//     console.log("Возраст слишком мал для посещения.");
+// }
+
+
+
